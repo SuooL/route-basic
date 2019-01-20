2008186abe69c6d913a4f3ab835a21f82fb6b2de
SuooL <1020935219@qq.com>
2019-01-20 22:08:28 +0800

    route-basic get demo

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,18 +3,9 @@ import Vapor
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
     // Basic "It works" example
-    router.get { req in
-        return "It works!"
+    router.get("demoGet", Int.parameter) {
+        req -> String in
+        let id = try req.parameters.next(Int.self)
+        return "hello get \(id)"
     }
-    
-    // Basic "Hello, world!" example
-    router.get("hello") { req in
-        return "Hello, world!"
-    }
-
-    // Example of configuring a controller
-    let todoController = TodoController()
-    router.get("todos", use: todoController.index)
-    router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
 }
